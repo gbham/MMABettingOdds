fe0ce7de8322f11cd545dfafd6ad5a2c9bd94953
Greg <gregbanham@hotmail.com>
2019-11-11 20:31:15 +0000

    Renamed some functions. Refactored the CreateTable() function some more to make it more readable. Implemented function ReverseOrderOfFightsPerEvent() to sort the order of the fights. This made the old process of assigning ID's to the rows not possible as it was done before the rows were reversed but now a new function called AssignRowIDs() has been added to resolve that issue.

--- server.js
+++ server.js
@@ -7,36 +7,6 @@ const waitUntil = require('wait-until');
 const request = require('request');
 
 
-
-// do {
-//     try{
-//         console.log("being ran");
-//         request('https://api.the-odds-api.com/v3/odds?api_key=1420768311f2ce13ca7ead7ea31c214d&sport=mma_mixed_martial_arts&region=uk', { json: true }, (err, res, body) => {
-                    
-//             global.oddsData = body;
-        
-//             if (err) { return console.log("Request module to get API DATA failed:" + err); }                             
-                        
-//         });
-//     }
-//     catch(err)
-//     {
-//         console.log("INSIDE THE CATCH FOR API CALL")
-//         request('https://api.the-odds-api.com/v3/odds?api_key=1420768311f2ce13ca7ead7ea31c214d&sport=mma_mixed_martial_arts&region=uk', { json: true }, (err, res, body) => {
-                    
-//             global.oddsData = body;
-        
-//             if (err) { return console.log("Request module to get API DATA failed:" + err); }                             
-                        
-//         });
-//     }
-    
-// }
-// while(global.oddsData!=undefined)
-
-
-
-
 app.use(express.static('public'));
 
 
@@ -49,19 +19,16 @@ const hbs = expbs.create({
     helpers: {
         
         
-        Sleep: function()   {
-            
+        Sleep: function()   {  
             var waitTill = new Date(new Date().getTime() + 4 * 1000);            
             while(waitTill > new Date()){}
 
         },
 
         Test: function()   {
-            console.log("API call made");
-            
+            console.log("API call made");           
         
-            try{     
-                
+            try{ 
                 
                 request('https://api.the-odds-api.com/v3/odds?api_key=41ca42613c3990833519f5b8a2b892e8&sport=mma_mixed_martial_arts&region=uk', { json: true }, (err, res, body) => {
                             
@@ -70,38 +37,34 @@ const hbs = expbs.create({
                 
                     if (err) { return console.log("Request module to get API DATA failed:" + err); }                             
                                 
-                });
-
-               
+                });               
               
             }
             catch(err)
             {
-                console.log("ERROR - INSIDE THE CATCH FOR API CALL")
-                                
-                // hbs.helpers.Test();
-
-                // request('https://api.the-odds-api.com/v3/odds?api_key=41ca42613c3990833519f5b8a2b892e8&sport=mma_mixed_martial_arts&region=uk', { json: true }, (err, res, body) => {
-                            
-                //     global.oddsData = body;
-                //     console.log("Just assinged global.oddsData");
+                console.log("ERROR - INSIDE THE CATCH FOR API CALL")                                
+                // hbs.helpers.Test(); //I believe in theory this should work. Not sure why it can't eventually recover, must be an underlying issue.
                 
-                //     if (err) { return console.log("Request module to get API DATA failed:" + err); }                             
-                                
-                // });
-            }       
-                
-            
-
-            
-
+            }
            
         },
 
+        CreateTable: function() { 
 
+            var rowsToInsertHeadings = hbs.helpers.WhereToInsertEventHeadings();
+
+            var rows = hbs.helpers.InputAPIDataIntoRows();       
+
+            rows = hbs.helpers.ReverseOrderOfFightsPerEvent(rows, rowsToInsertHeadings);
+
+            rows = hbs.helpers.AssignRowIDs(rows);
+
+            var table = hbs.helpers.InsertEventHeadings(rows, rowsToInsertHeadings);    
+            
+            return table;
+           
+        },
        
-
-
 
 
         WaitUntilAPIDataIsReturned: function()   {            
@@ -122,13 +85,72 @@ const hbs = expbs.create({
         //     } while (global.oddsData==undefined) //|| waitTill > new Date() WHEN THIS WAIT TILL IS USED, THE OTHER PART OF THE STATEMENT IS IGNORED
 
 
-
-
         },
 
+
+        WhereToInsertEventHeadings: function() {
+            var counter = 0;
+            var rowsToInsertHeadings = [];
+            for(let i = 0; i < global.oddsData.data.length -1 ; i++)  
+            {  
+                //Unix/Epoch time
+                var fightTimeA = global.oddsData.data[i].commence_time;
+                var fightTimeB = global.oddsData.data[i+1].commence_time;
+                var twelveHours = 43200;      
+                
+                //Since the API data is ordered by time if a fight is more than 12 hours away from the next one in the array, then they belong to differnt fight cards
+                if(fightTimeB - fightTimeA > twelveHours)
+                {
+                    //Why we need the "+2" above. For example, When i = 2, then we are actually at the 3rd data point of the oddsData.data[] array
+                    //This means we want (row[6]), the 7th row in the table, so we do i * 2 + 2 = row (e.g. 2 * 2 + 2 = 6) 
+                    rowsToInsertHeadings[counter] = i * 2 + 2;
+                    counter++; 
+                }
+            }
+
+            return rowsToInsertHeadings;
+        },
+
+
+        InputAPIDataIntoRows: function() {
+            //**NOTE: Keep in mind that rows[0] is where the fighters names/odds begin. The heading is not included in this count
+
+            global.homeOrAway = 0;
+            var team = 0; 
+            var rows = [];
+            var fight;
+                        
+            var NoOfRows = hbs.helpers.GetNumberOfRowsToShow();        
+
+            for (let i=0; i<NoOfRows; i++)  
+            {
+                if(i%2==0) 
+                {
+                    fight = i/2;                    
+                }
+                else 
+                {
+                    fight = (i-1)/2                    
+                }
+                
+                 
+                rows[i] = "<tr id=>";      //= "<tr id=row" + i + ">";             
+                rows[i] += "<td>" + global.oddsData.data[fight].teams[team] + "</td>";                
+                for (let column=1; column<10; column++) 
+                {                              
+                    rows[i] += "<td>" + hbs.helpers.LookupOdds(fight, column) + "</td>";                            
+                } 
+                rows[i] += "</tr>";                
+
+                if(team==0) {team=1} else {team=0};
+            }
+
+            return rows;
+        },
+
+
         //Any fights greater than 3 weeks away are not shown
-        GetNumberOfRowsToShow: function()   
-        {
+        GetNumberOfRowsToShow: function() {
             for(let i = 0; i < global.oddsData.data.length; i++)    
             {  
                 const now = Math.round(Date.now() / 1000);
@@ -142,106 +164,7 @@ const hbs = expbs.create({
             }
         },
 
-        WhereToInsertHeadings: function()
-        {
-            var counter = 0;
-            var rowsToInsertHeadings = [];
-            for(let i = 0; i < global.oddsData.data.length -1 ; i++)  
-            {  
-                //Unix/Epoch time
-                var fightTimeA = global.oddsData.data[i].commence_time;
-                var fightTimeB = global.oddsData.data[i+1].commence_time;
-                var twelveHours = 43200;      
-                
-                //Since the API data is ordered by time if a fight is more than 12 hours away from the next one in the array, then they belong to differnt fight cards
-                if(fightTimeB - fightTimeA > twelveHours)
-                {
-                //Why we need the "+2" above. For example, When i = 2, then we are actually at the 3rd data point of the oddsData.data[] array
-                //This means we want (row[6]), the 7th row in the table, so we do i * 2 + 2 = row (e.g. 2 * 2 + 2 = 6) 
-                rowsToInsertHeadings[counter] = i * 2 + 2;
-                counter++;     
 
-                }
-            }
-
-            return rowsToInsertHeadings;
-
-
-
-        },
-
-
-
-
-        CreateTable: function() { 
-
-            console.log("inside CreateTable");                        
-            
-            var rows = hbs.helpers.CreateRows();
-            var table = hbs.helpers.CreateTableFromRows(rows);    
-            
-            return table;           
-        },
-        
-        CreateRows: function() {  
-        //**NOTE: Keep in mind that rows[0] is where the fighters names/odds begin. The heading is not included in this count
-
-            global.homeOrAway = 0;
-            var rows = [];
-            var fight;
-            var team = 0; 
-            var NoOfRows = hbs.helpers.GetNumberOfRowsToShow();             
-            
-            for (let i=0; i<NoOfRows; i++)  
-            {
-                if(i%2==0) 
-                {
-                    fight = i/2;
-                }
-                else 
-                {
-                    fight = (i-1)/2
-                }
-                                                                                  
-                rows[i] = "<tr id=row" + i + ">";                
-                rows[i] += "<td>" + global.oddsData.data[fight].teams[team] + "</td>";                
-                for (let column=1; column<10; column++) 
-                {                              
-                    rows[i] += "<td>" + hbs.helpers.LookupOdds(fight, column) + "</td>";                            
-                } 
-                rows[i] += "</tr>";                
-
-                if(team==0) {team=1} else {team=0};
-            }
-            
-            return rows;  
-    },
-
-
-        CreateTableFromRows: function(rows) {
-        //**NOTE: Keep in mind that rows[0] is where the fighters names/odds begin. The heading is not included in this count
-            
-            //Insert main header row
-            let table ="<tr id=headerRow><th>Fight Card Title</th><th>Unibet</th><th>Ladbrokes</th><th>Betfred</th><th>Betfair</th><th>PaddyPower</th><th>Sport888</th><th>Matchbook</th><th>Marathon</th><th>NordicBet</th></tr>";
-            
-            var rowsToInsertHeadings = hbs.helpers.WhereToInsertHeadings(); 
-
-            //Appending the rows to create the final table string            
-            let counter = 0;
-            for (let i=0; i<rows.length; i++) 
-            {  
-                if(i==rowsToInsertHeadings[counter])
-                {   
-                    table += "<tr id=FightCardHeading><td>UFC 222: SMITH VS SMITH</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>"
-                    counter++;
-                }                
-                table += rows[i];
-            }        
-
-            return table;
-        },
-
-        
         LookupOdds: function(fight, column) { //, teams
                          
             var columnNumber = [
@@ -257,7 +180,7 @@ const hbs = expbs.create({
                 "nordicbet"                
             ]
         
-            //Since the function knows what column you are in, it can find out what bookies odds should be returned
+            //Since the function knows what column you are in, it can find out what bookmakers odds should be returned
             for(let i = 0; i < global.oddsData.data[fight].sites.length; i++)
             {   
                 if(global.oddsData.data[fight].sites[i].site_key == columnNumber[column])
@@ -266,7 +189,7 @@ const hbs = expbs.create({
                 }
             } 
 
-            //Since the home and away fighters will be alternating between calling this function, I can determine which one is calling here instead of sending 'teams' in as a paramter.
+            //Since the home and away fighters will be alternating between calling this function, I can determine which one is calling through this instead of sending 'teams' in as a paramter to the function
             if(global.homeOrAway == 0)
             {
                 global.homeOrAway = 1
@@ -277,6 +200,109 @@ const hbs = expbs.create({
             }
 
         },
+
+        //The order of the data from the API needs reversed
+        ReverseOrderOfFightsPerEvent: function(rows, rowsToInsertHeadings) {
+
+            //May need an EventFour at some point
+            var EventOne = [];
+            var EventTwo = [];
+            var EventThree = [];
+
+            for(let i=0; i<rows.length; i++)
+            {
+                if(i<rowsToInsertHeadings[0])
+                {
+                    EventOne[i] = rows[i];
+                }
+                else if (i>=rowsToInsertHeadings[0] && i<rowsToInsertHeadings[1])
+                {
+                    EventTwo[i] = rows[i];
+                }
+                else if (i>=rowsToInsertHeadings[1] && i<rowsToInsertHeadings[2])
+                {
+                    EventThree[i] = rows[i];
+                }
+            }
+
+            EventOne.reverse();
+            EventTwo.reverse();
+            EventThree.reverse();            
+            
+            for(let i=0; i<rows.length; i++)
+            {
+                if(i<EventOne.length)                
+                {
+                    rows[i] = EventOne[i];
+                }
+                else if (i<EventTwo.length)
+                {
+                    rows[i] = EventTwo[i];
+                }
+                else if (i<EventThree.length) 
+                {
+                    rows[i] = EventThree[i];
+                }
+            }
+
+            return rows;
+
+        },
+
+
+        AssignRowIDs: function(rows)  {
+            
+            for(let i=0; i<rows.length; i++)
+            {                
+                rows[i] = rows[i].substring(0,7) + "row" + i + rows[i].substring(7)
+
+            }
+
+            return rows;
+
+        },
+
+        InsertEventHeadings: function(rows, rowsToInsertHeadings)  {
+            //**NOTE: Keep in mind that rows[0] is where the fighters names/odds begin. The heading is not included in this count
+                
+                //Insert main header row
+                let table ="<tr id=headerRow><th>Fight Card Title</th><th>Unibet</th><th>Ladbrokes</th><th>Betfred</th><th>Betfair</th><th>PaddyPower</th><th>Sport888</th><th>Matchbook</th><th>Marathon</th><th>NordicBet</th></tr>";
+                    
+                //Appending the rows to create the final table string            
+                let counter = 0;
+                for (let i=0; i<rows.length; i++) 
+                {  
+                    if(i==rowsToInsertHeadings[counter])
+                    {   
+                        table += "<tr id=FightCardHeading><td>UFC 222: SMITH VS SMITH</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>"
+                        counter++;
+                    }                
+                    table += rows[i];
+                }        
+    
+                return table;
+        },
+
+
+        
+
+        
+
+
+        
+        
+
+        
+
+       
+
+
+        
+
+
+        
+        
+        
         
 
         
